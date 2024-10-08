7ad33d7b4110555c8026a4abc7904cbe0e00d55d
ZebaAnwer <165253475+ZebaAnwer@users.noreply.github.com>
2024-10-07 22:04:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var grocery = new Fruit("Mango", "Yellow", 200, 10);
+function Fruit(name, price, color, quantity) {
+    this.name = "Mango";
+    this.color = "Yellow";
+    this.price = 200;
+    this.quantity = 10;
+}
+console.log(grocery);
